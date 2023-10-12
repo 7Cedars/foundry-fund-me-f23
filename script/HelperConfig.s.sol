26437c6fe87b56fc5c5e039e9714f51eb341e610
Seven Cedars <Cedars7@proton.me>
2023-10-12 15:32:04 +0100

    Lesson 7.6

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,13 +2,26 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mock/MockV3Aggregator.sol";
 
-contract HelperConfig { 
+contract HelperConfig is Script { 
+  NetworkConfig public activeNetworkConfig;
 
   struct NetworkConfig {
     address priceFeed; // ETH/USD
   }
-  
+
+// if more networks, insert here. 
+  constructor() { 
+    if (block.chainid == 11155111){
+      activeNetworkConfig = getSepoliaEthConfig(); 
+    } else {
+      activeNetworkConfig = getAnvilEthConfig(); 
+    }
+  }
+
+
+  // this function can be copied to any network! 
   function getSepoliaEthConfig() public pure returns (NetworkConfig memory) { 
     // price feed address
     NetworkConfig memory sepoliaConfig = NetworkConfig({
@@ -17,8 +30,17 @@ contract HelperConfig {
     return sepoliaConfig;
   }
 
-  function getAnvilEthConfig() public pure returns (NetworkConfig memory) { 
-    
+  function getAnvilEthConfig() public returns (NetworkConfig memory) { 
+
+    vm.startBroadcast(); 
+    MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+    vm.stopBroadcast(); 
+
+    NetworkConfig memory anvilConfig = NetworkConfig({
+      priceFeed: address(mockPriceFeed)
+    });
+
+    return anvilConfig; 
   }
 
 }
